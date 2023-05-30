51a79c33f4819470fffe22d8c5e4fc45f2d7ebfd
Wren Ng <wrenng@big-turtle.attlocal.net>
2023-05-30 10:42:00 -0500

    create homepage handle bar

---
+++ server.js
@@ -0,0 +1,24 @@
+// Dependencies
+const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({helpers});
+
+// Sets up the Express App
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Set Handlebars as the default template engine.
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(require('./controllers/homeRoutes'));
+
+// Starts the server to begin listening
+app.listen(PORT, () => {
+  console.log('Server listening on: http://localhost:' + PORT);
+});

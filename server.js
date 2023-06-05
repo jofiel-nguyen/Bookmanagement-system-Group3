21db5a870ada235936132e8e89bcf4757d4dce66
Wren Ng <wrenng@big-turtle.attlocal.net>
2023-06-05 08:09:40 -0500

    fix bug for log in, search tool

--- server.js
+++ server.js
@@ -32,9 +32,30 @@ const users = JSON.parse(fs.readFileSync('./config/users.json'));
 app.use('/', homeRoutes.router);
 app.use('/', authRoutes.router);
 
-// Import and configure the login module
-const loginModule = require('./public/js/login');
-loginModule(app);
+// Login route
+app.get('/login', (req, res) => {
+  // Render the login form with the username
+  res.render('login', { username: req.session.user?.username });
+});
+
+// Handle login form submission
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  // Perform authentication logic here
+  const user = users.find((user) => user.username === username && user.password === password);
+  if (user) {
+    // Authentication success
+    req.session.user = { username };
+    req.session.loggedIn = true;
+    res.redirect('/');
+  } else {
+    // Authentication failed
+    // Render an error message or redirect back to the login page
+    res.send('Invalid username or password');
+  }
+});
+
 app.listen(PORT, () => {
   console.log('Server listening on: http://localhost:' + PORT);
 });
